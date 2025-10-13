7107931c747ce5e5a1c9a92c235a181da5986d7d
ali <awork1080@gmail.com>
2025-10-13 23:38:40 +0330

    making good...,

--- class/src/test/java/Cleasses.java
+++ class/src/test/java/Cleasses.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class Cleasses {
+
+    class Car {
+
+        boolean onn;
+        boolean open;
+
+        void on () {
+            System.out.println("Car is on");
+        }
+
+        void off () {
+            System.out.println("Car is off");
+        }
+
+
+        void open () {
+            System.out.println("Car is open");
+        }
+
+        void close () {
+            System.out.println("Car is close");
+        }
+
+        void start () {
+            if (onn && open) {
+                System.out.println("Car can start");
+            } else {
+                System.out.println("Car can't start");
+            }
+        }
+    }
 }
+
