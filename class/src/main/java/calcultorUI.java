0627c190b57a571dcfcfa1828de1528985f76e48
ali <awork1080@gmail.com>
2025-10-20 00:55:43 +0330

    calculator upgraded

--- class/src/main/java/calcultorUI.java
+++ class/src/main/java/calcultorUI.java
@@ -7,20 +7,28 @@ public class calcultorUI {
         JFrame calculator = new JFrame("Calculator");
 
         JTextField resault = new JTextField();
-        resault.setBounds(0, 0, 200, 50);
-        resault.setEnabled(false);
+        resault.setBounds(0, 0, 150, 50);
+        resault.setEditable(false);
         calculator.add(resault);
+
+        JButton remove = new JButton("C");
+        remove.setBounds(150, 0, 50, 50);
+        remove.addActionListener(e -> resault.setText(""));
+        calculator.add(remove);
 
         JButton one = new JButton("1");
         one.setBounds(0, 50, 50, 50);
+        one.addActionListener(e -> resault.setText(resault.getText() + one.getText()));
         calculator.add(one);
 
         JButton two = new JButton("2");
         two.setBounds(50, 50, 50, 50);
+        two.addActionListener(e -> resault.setText(resault.getText() + two.getText()));
         calculator.add(two);
 
         JButton three = new JButton("3");
         three.setBounds(100, 50, 50, 50);
+        three.addActionListener(e -> resault.setText(resault.getText() + three.getText()));
         calculator.add(three);
 
         JButton plus = new JButton("+");
@@ -29,14 +37,17 @@ public class calcultorUI {
 
         JButton four = new JButton("4");
         four.setBounds(0, 100, 50, 50);
+        four.addActionListener(e -> resault.setText(resault.getText() + four.getText()));
         calculator.add(four);
 
         JButton five = new JButton("5");
         five.setBounds(50, 100, 50, 50);
+        five.addActionListener(e -> resault.setText(resault.getText() + five.getText()));
         calculator.add(five);
 
         JButton six = new JButton("6");
         six.setBounds(100, 100, 50, 50);
+        six.addActionListener(e -> resault.setText(resault.getText() + six.getText()));
         calculator.add(six);
 
         JButton mines = new JButton("-");
@@ -45,15 +56,18 @@ public class calcultorUI {
 
         JButton seven = new JButton("7");
         seven.setBounds(0, 150, 50, 50);
+        seven.addActionListener(e -> resault.setText(resault.getText() + seven.getText()));
         calculator.add(seven);
 
         JButton eight = new JButton("8");
         eight.setBounds(50, 150, 50, 50);
+        eight.addActionListener(e -> resault.setText(resault.getText() + eight.getText()));
         calculator.add(eight);
 
 
         JButton nine = new JButton("9");
         nine.setBounds(100, 150, 50, 50);
+        nine.addActionListener(e -> resault.setText(resault.getText() + nine.getText()));
         calculator.add(nine);
 
         JButton mult = new JButton("*");
@@ -62,10 +76,12 @@ public class calcultorUI {
 
         JButton dot = new JButton(".");
         dot.setBounds(0, 200, 50, 50);
+        dot.addActionListener(e -> resault.setText(resault.getText() + dot.getText()));
         calculator.add(dot);
 
         JButton zero = new JButton("0");
         zero.setBounds(50, 200, 50, 50);
+        zero.addActionListener(e -> resault.setText(resault.getText() + zero.getText()));
         calculator.add(zero);
 
         JButton equal = new JButton("=");
@@ -78,7 +94,7 @@ public class calcultorUI {
 
 
 
-        calculator.setSize(220, 300);
+        calculator.setSize(300, 300);
         calculator.setLayout(null);
         calculator.setVisible(true);
 
