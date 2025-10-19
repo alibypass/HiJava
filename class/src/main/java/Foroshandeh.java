3358b412af9edebfd930ed0e02b84b016367fdfe
ali <awork1080@gmail.com>
2025-10-19 15:37:45 +0330

    statics added

---
+++ class/src/main/java/Foroshandeh.java
@@ -0,0 +1,15 @@
+public class Foroshandeh {
+
+    void foroshmashin () {
+        Car.sellscount++;
+    }
+
+    public static String[] sellers = new String[10];
+
+    static {
+        sellers[0] = "ali";
+        sellers[1] = "ahmad";
+
+    }
+
+}
