0dae9d16d7baf1d95a2ad31cac9a42113893e37c
ali <awork1080@gmail.com>
2025-10-27 21:24:40 +0330

    work with collections

--- class/src/test/java/Collection/list/ArrayList.java
+++ class/src/test/java/Collection/list/ArrayList.java
@@ -3,6 +3,8 @@ package Collection.list;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayList {
@@ -38,6 +40,45 @@ public class ArrayList {
             }
             return addedList;
         }
+    }
+
+
+    @Test
+    void should_reverse () {
+        List<String> animals = new java.util.ArrayList();
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("khar");
+
+        Collections.reverse(animals);
+
+        System.out.println(animals.indexOf("dog"));
+        System.out.println(animals.lastIndexOf("dog"));
+        Assertions.assertThat(animals.contains("dog")).isTrue();
+
+    }
+
+    @Test
+    void immutable () {
+        List<String> animals = new java.util.ArrayList();
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("khar");
+
+        List<String> immutable_animals = Collections.unmodifiableList(animals);
+
+        // immutable_animals.add("sss"); dont work
+        animals.add("snake");
+
+        Assertions.assertThat(animals.size()).isEqualTo(7);
+        Assertions.assertThat(immutable_animals.size()).isEqualTo(7); // true because refrece
+
     }
 
 
