350758baa013944ed8f1a7861b9aa3cc2c66914b
ali <awork1080@gmail.com>
2025-11-07 12:25:32 +0330

    sort objects added

--- class/src/test/java/compare/ComparableTest.java
+++ class/src/test/java/compare/ComparableTest.java
@@ -1,4 +1,29 @@
 package compare;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ComparableTest {
+
+    @Test
+    void slould_sort_mashin () {
+        MashinTest mashin_1 = new MashinTest("BMW", 550);
+        MashinTest mashin_2 = new MashinTest("BMW", 450);
+
+        List<MashinTest> mashins = new ArrayList<>();
+        mashins.add(mashin_1);
+        mashins.add(mashin_2);
+
+        Collections.sort(mashins);
+
+        List<MashinTest> exepctedmashins = new ArrayList<>();
+        exepctedmashins.add(mashin_1);
+        exepctedmashins.add(mashin_2);
+
+        Assertions.assertThat(mashins).isEqualTo(exepctedmashins);
+    }
 }
--- class/src/test/java/compare/MashinTest.java
+++ class/src/test/java/compare/MashinTest.java
@@ -1,4 +1,18 @@
 package compare;
 
-public class MashinTest {
+public class MashinTest implements Comparable<MashinTest> {
+
+    public int gonjayesh_bak;
+
+    MashinTest (String name, int gonjayesh_bak) {
+
+    }
+
+
+    @Override
+    public int compareTo(MashinTest o) {
+        int compare = Integer.compare(this.gonjayesh_bak, o.gonjayesh_bak);
+        
+        return compare;
+    }
 }
