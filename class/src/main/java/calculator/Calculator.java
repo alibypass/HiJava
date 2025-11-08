aacb431390096a687d6fb804b5b6db2ec60f7d34
ali <awork1080@gmail.com>
2025-11-08 16:30:51 +0330

    basic exception added

--- class/src/main/java/calculator/Calculator.java
+++ class/src/main/java/calculator/Calculator.java
@@ -3,8 +3,14 @@ package calculator;
 public class Calculator {
 
     public String add(String num1, String num2) {
-        double n1 = Double.parseDouble(num1);
-        double n2 = Double.parseDouble(num2);
+        double n1 = 0;
+        double n2 = 0;
+        try {
+            n1 = Double.parseDouble(num1);
+            n2 = Double.parseDouble(num2);
+        } catch (NumberFormatException nfe) {
+            System.out.println("number invalid");
+        }
         double result = n1 + n2;
         return String.valueOf(result);
     }
