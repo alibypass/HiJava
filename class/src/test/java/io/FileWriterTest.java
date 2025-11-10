a9f838c35abbe042667f8f9a317d5be0c9312104
ali <awork1080@gmail.com>
2025-11-10 22:15:30 +0330

    input write added

--- class/src/test/java/io/FileWriterTest.java
+++ class/src/test/java/io/FileWriterTest.java
@@ -2,6 +2,7 @@ package io;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,10 +10,22 @@ import java.nio.charset.StandardCharsets;
 public class FileWriterTest {
     @Test
     void should_write_in_file () {
-        try (FileOutputStream fileOutputStream = new FileOutputStream("text.txt");) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("test.txt");) {
             fileOutputStream.write("Hello from Java".getBytes(StandardCharsets.UTF_8));
+
+            FileInputStream fileInputStream = new FileInputStream("test.txt");
+            int hello = 0;
+            StringBuilder hello_world = new StringBuilder();
+            while ((hello = fileInputStream.read()) != -1) {
+
+                hello_world.append((char) hello);
+                //System.out.println(hello_world);
+            }
+            System.out.println(hello_world);
+
         } catch (IOException e) {
             System.out.println("error while works with file.txt");
+            e.printStackTrace();
         }
     }
 }
