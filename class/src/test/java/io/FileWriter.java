9d9f1f9306296da1f41f50875de679a1ae0d2643
ali <awork1080@gmail.com>
2025-11-10 21:25:23 +0330

    io added

--- class/src/test/java/io/FileWriter.java
+++ class/src/test/java/io/FileWriter.java
@@ -1,4 +1,21 @@
 package io;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class FileWriter {
+    @Test
+    void should_write_in_file () {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("text.txt");
+            fileOutputStream.write("Hello from Java".getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.out.println("error while works with file.txt");
+        }
+    }
 }
