59391a78c53f331778680a0c966e256abc97d0b5
ali <awork1080@gmail.com>
2025-11-15 00:08:03 +0330

    connection for database added

--- class/src/test/java/shop/ShoppingListDAOImplTest.java
+++ class/src/test/java/shop/ShoppingListDAOImplTest.java
@@ -1,4 +1,25 @@
 package shop;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ShoppingListDAOImplTest {
+
+    @Test
+    void should_connect_to_mysql_database () {
+        try (final Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "")) {
+            if (conn == null) {
+                Assertions.fail("connection failed");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Assertions.fail("connection failedd");
+
+        }
+    }
 }
