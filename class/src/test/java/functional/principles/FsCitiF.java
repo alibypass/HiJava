3133a7a57410b56faf9c9d7be9f1dce3b9b663d8
ali <awork1080@gmail.com>
2025-11-19 01:05:49 +0330

    ...

--- class/src/test/java/functional/principles/FsCitiF.java
+++ class/src/test/java/functional/principles/FsCitiF.java
@@ -1,4 +1,31 @@
 package functional.principles;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class FsCitiF {
+
+    @Test
+    void be_passed_as_method () {
+
+        List<String> players = new ArrayList<>();
+
+        players.add("messi");
+        players.add("ronaldo");
+        players.add("ali daei");
+
+        Collections.sort(players, (a, b) -> a.compareTo(b));
+
+
+        Comparator<String> variable = (a, b) -> a.compareTo(b);
+        Collections.sort(players, variable);
+
+
+
+    }
 }
