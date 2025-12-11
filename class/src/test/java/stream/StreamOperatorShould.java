366a83a716ad2cae3480596577988647a21f43e2
ali <awork1080@gmail.com>
2025-12-11 17:30:10 +0330

    map added

--- class/src/test/java/stream/StreamOperatorShould.java
+++ class/src/test/java/stream/StreamOperatorShould.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -28,4 +29,21 @@ public class StreamOperatorShould {
 
         Assertions.assertThat(topgoalnameali).isEqualTo(ali);
     }
+
+    @Test
+    void map_data () {
+        final List<Playerr> players = new PlayerHelper().getplayers();
+
+        Function<? super Playerr, ?> function = Playerr::getName;
+        List<?> playerNames = players.stream().map(function).collect(Collectors.toList());
+
+        final List<String> expectedresult = new LinkedList<>();
+        expectedresult.add("messi");
+        expectedresult.add("ali");
+
+
+        Assertions.assertThat(playerNames).isEqualTo(expectedresult);
+
+    }
 }
+
