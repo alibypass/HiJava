3f4e3642c7d8bf4260539bb6bc34c09bc5d8e6d7
ali <awork1080@gmail.com>
2025-12-11 16:20:47 +0330

    filter added

---
+++ class/src/test/java/stream/Playerr.java
@@ -0,0 +1,12 @@
+package stream;
+
+public class Player {
+    
+    public String name;
+    public int goal;
+
+    Player (String name, int goal) {
+        this.goal = goal;
+        this.name = name;
+    }
+}
